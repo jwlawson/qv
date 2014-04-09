916b1ce42f72b5856de0bf27ebc925c3d54707b7
John Lawson <john@jwlawson.co.uk>
2014-04-09 17:36:47 +0100

    Adds explicit hash and equal_to support for UnderlyingGraph shared_ptrs

--- include/underlying_graph.h
+++ include/underlying_graph.h
@@ -18,4 +18,39 @@ namespace cluster {
 
 	};
 }
+namespace std {
+	/* Add hash function to the std::hash struct. */
+	template <>
+	struct hash<cluster::UnderlyingGraph> {
+		size_t operator()(const cluster::UnderlyingGraph &x) const {
+			return x.hash();
+		}
+	};
+	/**
+	 * Annoyingly the standard shared_ptr hash function which passes the
+	 * function to the object in the pointer doesn't seem to work.
+	 */
+	template <>
+	struct hash<std::shared_ptr<cluster::UnderlyingGraph>> {
+		size_t operator()(const std::shared_ptr<cluster::UnderlyingGraph> &x)
+				const {
+			return x->hash();
+		}
+	};
+	/* Add equals function to std::equal_to */
+	template<>
+	struct equal_to<cluster::UnderlyingGraph> {
+		bool operator()(const cluster::UnderlyingGraph &lhs,
+		                const cluster::UnderlyingGraph &rhs) const {
+			return lhs.equals(rhs);
+		}
+	};
+	template<>
+	struct equal_to<std::shared_ptr<cluster::UnderlyingGraph>> {
+		bool operator()(const std::shared_ptr<cluster::UnderlyingGraph> &lhs,
+		                const std::shared_ptr<cluster::UnderlyingGraph> &rhs) const {
+			return lhs->equals(*rhs);
+		}
+	};
+}
 

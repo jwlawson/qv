ec1b7b59c2b85a68dfe95f001b5e3ba79083c55b
John Lawson <john@jwlawson.co.uk>
2014-04-10 14:05:59 +0100

    Adds new testcase

--- test/slow_mmi_test.cc
+++ test/slow_mmi_test.cc
@@ -6,6 +6,7 @@
 #include "gtest/gtest.h"
 #include "mmi.h"
 #include "dynkin.h"
+#include <string>
 
 namespace cluster {
 
@@ -36,6 +37,20 @@ namespace cluster {
 		QuiverMatrix m(5,5,v);
 
 		EXPECT_FALSE(mmi::slow_mmi(m));
+	}
+
+	TEST(SlowMMI, NonHyperbolic) {
+		std::string s = "{ { 0 -1 1 0 1 } { 1 0 -1 0 -1 } { -1 1 0 1 0 } { 0 0 -1 0 1 } { -1 1 0 -1 0 } }";
+		QuiverMatrix a(s);
+
+		EXPECT_TRUE(mmi::slow_mmi(a));
+	}
+
+	TEST(SlowMMI, 4x4) {
+		std::string s = "{ { 0 -1 0 1 } { 1 0 1 1 } { 0 -1 0 1 } { -1 -1 -1 0 } }";
+		QuiverMatrix a(s);
+
+		EXPECT_TRUE(mmi::slow_mmi(a));
 	}
 }
 

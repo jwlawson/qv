d99f3305c894e06502d83a37369be7803726d8c3
John Lawson <john@jwlawson.co.uk>
2014-06-10 14:11:47 +0100

    Adds infinite quiver test case.

--- test/mutation_class_test.cc
+++ test/mutation_class_test.cc
@@ -17,4 +17,12 @@ namespace cluster {
 		EXPECT_EQ(4, c.size());
 		EXPECT_TRUE(c.contains(std::make_shared<EquivQuiverMatrix>(mat)));
 	}
+
+	TEST(MutationClass, Infinite) {
+		int v[] = {0,1,0,0, -1,0,1,1, 0,-1,0,1, 0,-1,-1,0};
+		EquivQuiverMatrix m(4,4,v);
+		MutationClass c(m);
+
+		EXPECT_FALSE(c.is_finite());
+	}
 }

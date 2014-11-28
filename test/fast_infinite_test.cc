943326b3bb7023d48b6e3ea840b79ebf6682acc4
John Lawson <john@jwlawson.co.uk>
2014-11-28 10:50:22 +0000

    Formatting clean up.

--- test/fast_infinite_test.cc
+++ test/fast_infinite_test.cc
@@ -1,11 +1,10 @@
 /*
  * fast_infinite_test.cc
  */
-
 #include "gtest/gtest.h"
+
 #include "dynkin.h"
 #include "fast_infinite.h"
-#include <iostream>
 
 namespace cluster {
 
@@ -13,18 +12,15 @@ namespace cluster {
 		QuiverMatrix matrix = dynkin::A5;
 		EXPECT_FALSE(fastinf::is_infinite(matrix));
 	}
-
 	TEST(FastInfinite, G2) {
 		QuiverMatrix matrix = dynkin::G2;
 		EXPECT_FALSE(fastinf::is_infinite(matrix));
 	}
-
 	TEST(FastInfinite, Inf) {
 		int v[] = { 0, 1, 0, 0, -1, 0, 1, 1, 0, -1, 0, 1, 0, -1, -1, 0};
 		QuiverMatrix mat(4, 4, v);
 		EXPECT_TRUE(fastinf::is_infinite(mat));
 	}
-
 	TEST(FastInfinite, Infinite5) {
 		int v[] = { 0, -1, 1, 0, 2, 1, 0, -1, 0, -2, -1, 1, 0, 1, 2, 0, 0, -1, 0, 0, -2,
 		            2, -2, 0, 0
@@ -32,12 +28,10 @@ namespace cluster {
 		QuiverMatrix mat(5, 5, v);
 		EXPECT_TRUE(fastinf::is_infinite(mat));
 	}
-
 	TEST(FastInfinite, Empty) {
 		QuiverMatrix empty;
 		EXPECT_FALSE(fastinf::is_infinite(empty));
 	}
-
 	TEST(FastInfinite, Small) {
 		int v[] = {0};
 		QuiverMatrix small(1, 1, v);

660f2c1555fa1f7a5042720b8f1e2a720194a4ed
John Lawson <john@jwlawson.co.uk>
2015-09-14 16:24:54 +0100

    Adds static cast to bool required for explicit operator (new GCC).

--- src/stream_iterator.cc
+++ src/stream_iterator.cc
@@ -31,14 +31,14 @@ namespace cluster {
 	template<class T>
 	StreamIterator<T>::StreamIterator(std::istream& stream)
 		: input_(&stream, NullDeleter()) {
-		has_next_ = std::getline(*input_, str_);
+		has_next_ = static_cast<bool>(std::getline(*input_, str_));
 	}
 
 	template<class T>
 	std::shared_ptr<T> StreamIterator<T>::next() {
 		std::shared_ptr<T> result = std::make_shared<T>(str_);
 		do {
-			has_next_ = std::getline(*input_, str_);
+			has_next_ = static_cast<bool>(std::getline(*input_, str_));
 		} while (has_next_ && str_[0] != '{');
 		return std::move(result);
 	}
